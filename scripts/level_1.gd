c9d99640c1687316a93e708d0e4647d926b16799
Marco <marco.gimena@student.ateneo.edu>
2025-09-08 23:08:31 +0800

    Laid out foundations for level 1 and level structure guide for future levels - Implemented basic detection of lose condition - Improved logic on hazard management. Disabling hazards added - Reformatted script structure. Grouped relevant functions together - Applied scene switching after level completion/failure - Applied scene resetting after lose condition met

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -6,25 +6,87 @@ extends XRToolsSceneBase
 ## specifically for Level 1
 ##
 ## This could serve as a guide/reference for future level creation  
+##
+## Handles initialization, hazards, objectives, and level flow.
 
-var hazards
-var shakers
-var world_shaker
+var hazards : Node
+var shakers : Node
+var world_shaker : Node
+var objectives : Node
+
+var level_active = false
+var level_timer : float = 0.0
+var score : int = 0 # Temporary basic scoring system 
 
 func _ready():
-	# Getting appropriate nodes
+	start_level()
+
+### LEVEL LIFECYCLE ###
+
+func start_level():
+	print("Level started")
+	level_active = true
+	level_timer = 0
+	
+	# Cache references
 	hazards = get_node("Hazards")
 	shakers = get_node("Shakers")
 	world_shaker = get_node("WorldShaker")
+	objectives = get_node("Objectives")
+
+	enable_hazards()
+	enable_objectives()
+
+func end_level(success: bool):
+	level_active = false
+	# disable hazards
 	
+	if success:
+		print("Level complete! Score: %s" % score)
+	else:
+		print("Level failed! Score: %s" % score)
+		exit_to_main_menu()
+	
+	# TODO: trigger scene switch or results UI
+
+
+### HAZARDS ###
+
+func enable_hazards():
 	# For connecting all hazard signals to call function _on_hazard_triggered(name)
+	if not hazards: return
 	for hazard in hazards.get_children():
-		hazard.hazard_triggered.connect(_on_hazard_triggered)
-	
-	# Connecting shakers to worlshaker
-	for shaker in shakers.get_children():
-		shaker.hazard_triggered.connect(world_shaker.on_shake_hazard_triggered)
+		if hazard.has_signal("hazard_triggered"):
+			hazard.hazard_triggered.connect(_on_hazard_triggered)
+
+func disable_hazards():
+	if not hazards: return
+	for hazard in hazards.get_children():
+		if hazard.has_signal("hazard_triggered"):
+			if hazard.hazard_triggered.is_connected(_on_hazard_triggered):
+				hazard.hazard_triggered.disconnect(_on_hazard_triggered)
 
 # Temporary function
 func _on_hazard_triggered(hazard_name: String):
-	print("Hazard: " + hazard_name + " has been triggered!")
+	print("Hazard: %s triggered!" % hazard_name)
+	reset_scene()
+	
+	# TODO: log hazard for feedback
+
+
+### OBJECTIVES ###
+
+func enable_objectives():
+	# TODO: implement objectives
+	return
+
+
+### PROCESS LOOP ###
+
+func _process(delta: float) -> void:
+	if level_active:
+		level_timer += delta
+		# Example: fail condition after 120s
+		if level_timer > 120.0:
+			end_level(false)
+	
