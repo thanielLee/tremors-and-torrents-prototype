354c3ce4ad17ec9d5225e16ec0a00d090af4c670
larcomicious <marco.gimena@student.ateneo.edu>
2025-09-19 13:04:33 +0800

    Improved Jammed Door object to include hit points
    - Added hit sound and break sound

--- scripts/jammed_door.gd
+++ scripts/jammed_door.gd
@@ -1,23 +1,49 @@
 extends Node3D
 
 signal door_cleared()
+
 @onready var mesh: Node3D = $Mesh
+@onready var break_door_sound: AudioStreamPlayer3D = $BreakDoorSound
 @onready var break_sound: AudioStreamPlayer3D = $BreakSound
+@onready var hit_points: Node3D = $HitPoints
+@onready var hit_sound: AudioStreamPlayer3D = $HitSound
+
+var hit_point_count : int
 
 
 func _ready() -> void:
-	#if EventManager.completed_events.has("ActionRemoveJammedDoor_Done"):
-		#queue_free()
-	pass
-
-func _on_ois_strike_receiver_action_completed(requirement: Variant, total_progress: Variant) -> void:
-	emit_signal("door_cleared")
-	mesh.visible = false
+	hit_point_count = hit_points.get_child_count()
 	
+	for hit_point in hit_points.get_children():
+		for child in hit_point.get_children():
+			if child is OISStrikeReceiver:
+				child.action_completed.connect(_on_hit_point_completed.bind(hit_point)) # for hit point desrtoyed
+				child.action_started.connect(_on_hit_point_started)
+		
 
-func _on_ois_strike_receiver_action_started(requirement: Variant, total_progress: Variant) -> void:
-	print("started")
+func check_finished():
+	if hit_point_count <= 0:
+		print("Door cleared")
+		emit_signal("door_cleared")
+		mesh.visible = false
+		# TODO: implement break door sound and particles
+
+func _on_hit_point_started(requirement: Variant, total_progress: Variant) -> void:
+	if hit_sound and not hit_sound.playing:
+		hit_sound.play()
+	## TODO: implement hit particles
+
+func _on_hit_point_completed(requirement: Variant, total_progress: Variant, hit_point: Node) -> void:
+	print("HitPoint cleared: ", hit_point.name)
+	
+	hit_point.queue_free()
+	hit_point_count -= 1
+	
+	if break_sound:
+		break_sound.play()
+	
+	check_finished()
 
 
-func _on_break_sound_finished() -> void:
+func _on_break_door_sound_finished() -> void:
 	queue_free()
