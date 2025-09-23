b1aadbe6960c99e4f46a22ff7b3c695c11ce6add
larcomicious <marco.gimena@student.ateneo.edu>
2025-09-23 15:27:47 +0800

    Added rumble to world shake. Needs testing

--- scripts/world_shaker.gd
+++ scripts/world_shaker.gd
@@ -4,6 +4,9 @@ extends Node3D
 @export var shake_duration := 0.5
 @export var shake_intensity := 0.1
 var time_left = 0.0
+@onready var xr_tools_rumbler: XRToolsRumbler = $XRToolsRumbler
+@onready var xr_tools_rumbler_2: XRToolsRumbler = $XRToolsRumbler2
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -21,6 +24,9 @@ func _process(delta):
 
 func shake_world():
 	time_left = shake_duration
+	xr_tools_rumbler.rumble()
+	xr_tools_rumbler_2.rumble()
+	
 
 func _on_level_1_shake_world() -> void:
 	shake_world()
