f865d7df8d40ff4b4f9256353ff5e068bf738f39
alls-sose <9815623+alls-sose@users.noreply.github.com>
2025-11-26 15:02:22 +0800

    Integrated assets - Applied mesh to glass_shards hazard - Turned off rumble temporarily - Applied light to electrical_fire hazard - Cleaned glass_shards mesh

--- addons/godot-xr-tools/rumble/rumble_manager.gd
+++ addons/godot-xr-tools/rumble/rumble_manager.gd
@@ -100,14 +100,14 @@ func _process(delta: float) -> void:
 		magnitude *= XRToolsUserSettings.haptics_scale
 
 		# Make that tracker rumble
-		if magnitude > 0:
-			XRServer.primary_interface.trigger_haptic_pulse(
-				HAPTIC_ACTION,
-				tracker_name, # if the tracker name isn't valid, it will error but continue
-				0,
-				magnitude,
-				0.1,
-				0)
+		#if magnitude > 0:
+#s			XRServer.primary_interface.trigger_haptic_pulse(
+				#HAPTIC_ACTION,
+				#tracker_name, # if the tracker name isn't valid, it will error but continue
+				#0,
+				#magnitude,
+				#0.1,
+				#0)
 
 
 # Add an event
